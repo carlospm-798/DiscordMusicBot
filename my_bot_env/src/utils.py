d0854c86f030b77a4d0fc3f01f40e2f7397b44dc
Carlos Paredes <carlos.paredes6900@alumnos.udg.mx>
2025-05-24 16:24:41 -0600

    Adding utils.py and finishing its documentation

--- my_bot_env/src/utils.py
+++ my_bot_env/src/utils.py
@@ -1,7 +1,19 @@
 import wavelink
 from discord.ext import commands
 
+#   --------------------------------------------------------------------------      #
+#   wavelink:           Is the client that interact with the Lavalink server,       #
+#                       and reproduces audio.                                       #
+#   commands:           It's a submodule that facilitates the creation of bots,     #
+#                       based in commands and cogs.                                 #
+#   --------------------------------------------------------------------------      #
+
 MAX_TRACKS = 100
+
+#   --------------------------------------------------------------------------      #
+#   MAX_TRACKS:         Is a constant that defines the max value of songs in        #
+#                       total, to avoid gigant lists.                               #
+#   --------------------------------------------------------------------------      #
 
 async def get_player(ctx) -> wavelink.Player:
     if isinstance(ctx.voice_client, wavelink.Player):
@@ -11,3 +23,8 @@ async def get_player(ctx) -> wavelink.Player:
         return await ctx.author.voice.channel.connect(cls=wavelink.Player)
 
     raise commands.CommandError("You must be in a voice channel.")
+
+#   --------------------------------------------------------------------------      #
+#   get_player:         It's an async helper that unify all the lógic               #
+#                       to create or use the audio reproduction for the bot.        #
+#   --------------------------------------------------------------------------      #
